991f0d5819012d7480e55a981a712f743c4c952c
Sugarrrrrrrrrr <Sugarrrrrrrrrr@outlook.com>
2017-12-28 15:31:43 +0800

    20171228

--- test_udp/udpClient.py
+++ test_udp/udpClient.py
@@ -2,7 +2,7 @@
 
 from socket import *
 
-HOST = '192.168.42.128'
+HOST = '10.168.103.255'
 PORT = 14550
 BUFSIZE = 1024
 
@@ -11,15 +11,15 @@ ADDR = (HOST, PORT)
 udpCliSock = socket(AF_INET, SOCK_DGRAM)
 
 for i in range(100):
-    #data = input('>')
-    data = '%2d:hello' % i
+    data = input('>')
+    #data = '%2d:hello' % i
     if not data:
         break
     udpCliSock.sendto(data.encode('utf-8'), ADDR)
-    # data, ADDR = udpCliSock.recvfrom(BUFSIZE)
-    # if not data:
-    #     break
-    # print(data)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZE)
+    if not data:
+        break
+    print(data)
 
 
 udpCliSock.close()
